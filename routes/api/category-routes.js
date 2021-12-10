a9d3539baebcf0ba4ac63084a07f4a52e3018b5f
kngurley <kngurley25@gmail.com>
2021-12-10 16:59:37 -0600

    add put route for Category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -51,6 +51,22 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbCategoryData => {
+      if (!dbCategoryData) {
+        res.status(404).json({ message: 'No category found with that ID'});
+        return;
+      }
+      res.json(dbCategoryData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    })
 });
 
 router.delete('/:id', (req, res) => {
