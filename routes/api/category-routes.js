4ad1edaa636ab3691a3a860bb953e7f33ab99d5c
kngurley <kngurley25@gmail.com>
2021-12-10 18:14:54 -0600

    add delete route for Category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -71,6 +71,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbCategoryData => {
+    if (!dbCategoryData) {
+      res.status(404).json({ message: 'No category found with that ID'});
+      return;
+    }
+    res.json(dbCategoryData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 module.exports = router;
